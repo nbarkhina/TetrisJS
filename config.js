7a174005a325d59cf8141122e5fb675f4da7e19b
Neil Barkhina <nbarkhina@gmail.com>
2018-12-30 22:31:15 -0500

    added no caching for js

--- config.js
+++ config.js
@@ -1,6 +1,13 @@
 requirejs.config({
     paths: {
     // "angular": "/node_modules/angular/angular"
+    },
+    urlArgs: function (id, url) {
+        var args = '';
+        if (url.indexOf('index') !== -1) {
+            args = '?v=1';
+        }
+        return args;
     }
 });
 require(["index"]);
--- config.ts
+++ config.ts
@@ -3,6 +3,13 @@ declare var requirejs, require;
 requirejs.config({
     paths: {
         // "angular": "/node_modules/angular/angular"
+    },
+    urlArgs: function(id, url) {
+        var args = '';
+        if (url.indexOf('index') !== -1) {
+            args = '?v=1'
+        }
+        return args;
     }
 });
 
